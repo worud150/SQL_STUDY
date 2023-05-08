f0237abf2fd125cde4f666c25c6da51cfe95dbbf
worud150 <130972442+worud150@users.noreply.github.com>
2023-05-08 17:28:26 +0900

    Add files via upload

---
+++ Mysql 자료형.sql
@@ -0,0 +1,15 @@
+SELECT NOW() FROM DUAL;
+
+-- char(최대값이255자까지 저장가능) 
+-- > 고정길이 무조건 10개만 사용 속도가 더 빠름 
+-- varchar(65,535자 저장가능)  
+-- > 가변길이 
+-- text(65,553자) 세개다 가변길이 
+-- mediumtext(2)
+-- longtext(3)
+
+SHOW character SET;
+
+
+-- tinyint 
+
